61b2b26cbb62f78791f17400862513f885bd875e
snehasatheesan <snehasatheesan13@gmail.com>
2024-08-16 15:45:40 +0530

    Create Train.java

---
+++ Code2/Train.java
@@ -0,0 +1,10 @@
+package MethodOverriding;
+public class train extends vehicle{
+    
+    public void start() {
+        System.out.println("The train started");
+    }
+    public void stop() {
+        System.out.println("The train stopped");
+    }
+}
