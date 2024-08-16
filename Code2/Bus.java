7a29547308b305f7a27c444e632f9ac15f01480e
snehasatheesan <snehasatheesan13@gmail.com>
2024-08-16 15:44:11 +0530

    Create Bus.java

---
+++ Code2/Bus.java
@@ -0,0 +1,11 @@
+package MethodOverriding;
+public class bus extends vehicle{
+	    public void start() {
+	        System.out.println("The bus started");
+	    }
+
+	    public void stop() {
+	        System.out.println("The bus stopped");
+	    }
+	}
+
