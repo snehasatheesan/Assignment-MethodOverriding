b7565ac95fecd9795c7ab96df035c55d1cfec893
snehasatheesan <snehasatheesan13@gmail.com>
2024-08-16 15:42:18 +0530

    Create Vehicle.java

---
+++ Code2/Vehicle.java
@@ -0,0 +1,12 @@
+package MethodOverriding;
+
+public class vehicle {
+	
+	    public void start(){
+	        System.out.println("The vehicle started");
+	    }
+	    public void stop(){
+	        System.out.println("The vehicle stopped");
+	    }
+
+	}
