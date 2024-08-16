f4e5f4f0a5595d1f261cad8fd73f62a4d47ffdf8
snehasatheesan <snehasatheesan13@gmail.com>
2024-08-16 15:49:39 +0530

    Update VehicleDemo.java

--- Code2/VehicleDemo.java
+++ Code2/VehicleDemo.java
@@ -1,2 +1,18 @@
 2. In Vehicle hierarchy, add methods like start and stop in Vehicle and override them in Child classes.
   
+package Inheritence;
+
+public class vehicleDemo {
+	    public static void main(String[] args) {
+	        vehicle sc=new vehicle();
+	        train a=new train();
+	        bus b=new bus();
+	        sc.start();
+	        sc.stop();
+	        a.start();
+	        a.stop();
+	        b.start();
+	        b.stop();
+	    }
+	}
+
